9c97e2c2dea93962c32ae662525a80ca383ac8a7
fredikw <fred.i.kwizera@gmail.com>
2022-12-22 23:09:56 +0100

    print_transactions API

--- stock-predictions/main.py
+++ stock-predictions/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def print_transactions(m: float, k: int, d: int, names: np.ndarray, owned: np.ndarray) -> str:
+def print_transactions(m: float, k: int, d: int, names: np.ndarray, owned: np.ndarray, prices: np.ndarray) -> str:
     """Print the transactions for a given day.
 
     Parameters:
@@ -11,11 +11,12 @@ def print_transactions(m: float, k: int, d: int, names: np.ndarray, owned: np.nd
     - d: The number of days for which stock prices are received.
     - names: An array of the names of the stocks.
     - owned: An array of the number of stocks owned for each stock.
+    - prices: An array of the prices for the stocks for the current and last four days
 
     Returns:
     - A string representing the transactions for the day.
     """
-    return ""
+    return "2 \nUFL BUY 1\nUCB BUY 1"
 
 def main():
     """Read data from a file, process it, and print the transactions."""
@@ -34,20 +35,43 @@ def main():
 
     d_tot = data.count()
     names = data.columns.to_numpy()
-    owned = np.array([])
+    owned = np.zeros(k)
 
     # Iterate over the rows of the dataframe
     for i, row in data.iterrows():
-        if i < 5:
+        if i < 4:
             continue
 
         d = d_tot - i
         prices = data[i-n:i].to_numpy().transpose()
 
-        print_transactions(m, k, d, names, owned)
+        orders = print_transactions(m, k, d, names, owned, prices)
 
-        print(row)
+        # Executing order
+        for i, order in enumerate(orders.splitlines()):
+            if i == 0: continue
 
+            stock, order_type, num_shares = order.split()
+
+            num_shares = int(num_shares)
+            
+            stock_price = row.loc[stock]
+
+            name_idx = np.argwhere(names == stock)[0][0]
+
+            if order_type == "SELL":
+
+                m += stock_price*num_shares
+
+                owned[name_idx] -= num_shares
+
+            if order_type == "BUY":
+
+                if stock_price*num_shares <= m:
+
+                    m -= stock_price*num_shares
+
+                    owned[name_idx] += num_shares
 
 if __name__ == "__main__":
     main()
