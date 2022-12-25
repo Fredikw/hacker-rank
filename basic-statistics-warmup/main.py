466e935733a69bdc33dab4b28ff86232d88c9941
fredikw <fred.i.kwizera@gmail.com>
2022-12-25 22:14:34 +0100

    Basic Statistics Warmup complete

---
+++ basic-statistics-warmup/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+from statistics import mode
+from scipy.stats import norm
+
+if __name__ == "__main__":
+
+    # Read input from STDIN
+    # input = input("Sample Input: ")
+    input = """5
+    12 43 11 23 33
+    """
+
+    values = np.array(input.splitlines()[1].split(), dtype=int)
+    
+    conf_interval = norm.interval(0.95, loc=np.mean(values), scale=np.std(values))
+
+    print(np.mean(values))
+    print(np.median(values))
+    print(mode(values))
+    print(np.std(values))
+    print(conf_interval[0], conf_interval[1])
